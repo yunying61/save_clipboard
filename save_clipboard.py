071f45aee49d8fca98cb47946aeafb4c7933a63f
yunying61 <51905599+yunying61@users.noreply.github.com>
2021-10-07 11:06:54 +0800

    1.新增了输出日志的功能 2.新增了各模块将异常输出为日志的功能 3.将剪贴板内容保存至程序运行目录下的子目录中
    下次功能预览：
    将取消命令窗口的运行方式，转换成程序运行窗口的形式

--- save_clipboard.py
+++ save_clipboard.py
@@ -20,13 +20,13 @@ def save_to_log(log):
         # 获取时间
         t = time.strftime('%Y-%m-%d-%H:%M:%S')
         # 打开 save_clipboard.log 文件，写入日志
-        with open('.\\log\\save_clipboard.log', 'a+', encoding='utf-8') as f:
+        with open('.\\log\\save_clipboard.log', 'a+') as f:
             f.write('[' + t + ']    ' + log + '\n')
-    except Exception as e:
+    except Exception as e_save_to_log:
         t = time.strftime('%Y-%m-%d-%H:%M:%S')
         # 打开 save_clipboard.log 文件，写入错误日志
-        with open('.\\log\\save_clipboard.log', 'a+', encoding='utf-8') as f:
-            f.write('[' + t + ']    ' + '日志写入失败！错误内容：' + str(e) + '\n')
+        with open('.\\log\\save_clipboard.log', 'a+') as f:
+            f.write('[' + t + ']    ' + '日志写入失败！错误内容：' + str(e_save_to_log) + '\n')
 
 
 # 获取剪切板的内容
@@ -40,25 +40,32 @@ def get_clipboard():
         wc.CloseClipboard()
         # 返回获取的内容
         return str(data)
-    except Exception as e:
+    except Exception as e_get_clipboard:
         t = time.strftime('%Y-%m-%d-%H:%M:%S')
         # 打开 save_clipboard.log 文件，写入错误日志
-        with open('.\\log\\save_clipboard.log', 'a+', encoding='utf-8') as f:
-            f.write('[' + t + ']    ' + '剪贴板内容获取失败！错误内容：' + str(e) + '\n')
+        with open('.\\log\\save_clipboard.log', 'a+') as f:
+            f.write('[' + t + ']    ' + '剪贴板内容获取失败！错误内容：' + str(e_get_clipboard) + '\n')
 
 
 # 将内容存储到本地文件
 def save_to_file(data):
+    # 确认是否有 Clipboard 目录
+    exist_dir = os.path.exists('Clipboard')
+    if exist_dir is False:
+        # 如果没有 Clipboard 目录则创建
+        os.mkdir('Clipboard', 3)
     try:
-        t = time.strftime('%Y-%m-%d')
-        with open('剪切板内容-{}.txt'.format(t), 'a+', encoding='utf-8') as f:
-            f.write(data + '\n' + '\n')
-            print('已保存！')
-    except Exception as e:
+        t1 = time.strftime('%Y-%m-%d')
+        t2 = time.strftime('%Y-%m-%d-%H:%M:%S')
+        with open('.\\Clipboard\\剪切板内容-{}.txt'.format(t1), 'a+') as f:
+            f.write(t2 + '----------------------------------------\n' + data + '\n' + '\n')
+        with open('.\\log\\save_clipboard.log', 'a+') as f:
+            f.write('[' + t2 + ']    ' + '剪贴板内容保存成功' + '\n')
+    except Exception as e_save_to_file:
         t = time.strftime('%Y-%m-%d-%H:%M:%S')
         # 打开 save_clipboard.log 文件，写入错误日志
-        with open('.\\log\\save_clipboard.log', 'a+', encoding='utf-8') as f:
-            f.write('[' + t + ']    ' + '文件保存失败！错误内容：' + str(e) + '\n')
+        with open('.\\log\\save_clipboard.log', 'a+') as f:
+            f.write('[' + t + ']    ' + '文件保存失败！错误内容：' + str(e_save_to_file) + '\n')
 
 
 if __name__ == '__main__':
@@ -70,8 +77,8 @@ if __name__ == '__main__':
         last_data = get_clipboard()
 
         while True:
-            # 每隔60s读取剪切板的内容
-            time.sleep(60)
+            # 每隔2s读取剪切板的内容
+            time.sleep(2)
             # 读取剪切板内容
             now_data = get_clipboard()
             # 如果当前的内容与上一时刻的内容不一致,则写入文件
@@ -80,8 +87,8 @@ if __name__ == '__main__':
 
                 # 用当前的剪切板内容替换为上一时刻的内容,等待下一次的复制、剪切
                 last_data = now_data
-    except Exception as e:
-        t = time.strftime('%Y-%m-%d-%H:%M:%S')
+    except Exception as e_main:
+        t_main = time.strftime('%Y-%m-%d-%H:%M:%S')
         # 打开 save_clipboard.log 文件，写入错误日志
-        with open('.\\log\\save_clipboard.log', 'a+', encoding='utf-8') as f:
-            f.write('[' + t + ']    ' + '程序运行出错！错误内容：' + str(e) + '\n')
+        with open('.\\log\\save_clipboard.log', 'a+') as f_main:
+            f_main.write('[' + t_main + ']    ' + '程序运行出错！错误内容：' + str(e_main) + '\n')
